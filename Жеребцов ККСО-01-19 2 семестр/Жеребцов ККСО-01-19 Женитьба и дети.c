25fb8020559cef8780e77dcff8628f5a782d0659
Ozzero Game <kirillze20@gmail.com>
2020-02-22 14:03:08 +0300

    Create Жеребцов ККСО-01-19 Женитьба и дети.c

---
+++ Жеребцов ККСО-01-19 2 семестр/Жеребцов ККСО-01-19 Женитьба и дети.c
@@ -0,0 +1,34 @@
+﻿/*Жеребцов ККСО-01-19 Женитьба и дети*/
+#include <stdio.h>
+
+struct Person
+{
+	char surname[16];
+	char married;
+	char children;
+};
+
+
+int main()
+{
+	struct Person data[25];
+
+	printf("Surname / Married (y or n) / Children (y or n)\n\n");
+
+	for (int i = 0; i < 25; i++)
+	{
+		scanf("%s %c %c", data[i].surname, &data[i].married, &data[i].children);
+	}
+
+	printf("----- Married and Children -----\n\n");
+
+	for (int i = 0; i < 25; i++)
+	{
+		if (data[i].married == 'y' && data[i].children == 'y')
+		{
+			printf("Mr/Mrs %s married and has children\n", data[i].surname);
+		}
+	}
+
+	return 0;
+}
