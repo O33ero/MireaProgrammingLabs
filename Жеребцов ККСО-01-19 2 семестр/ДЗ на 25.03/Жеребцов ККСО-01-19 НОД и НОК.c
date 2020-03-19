d3cca3fa19bcdb63ee461408aa7486ba3e146c13
Ozzero Game <kirillze20@gmail.com>
2020-03-19 11:17:46 +0300

    + дз на 25.03

---
+++ Жеребцов ККСО-01-19 2 семестр/ДЗ на 25.03/Жеребцов ККСО-01-19 НОД и НОК.c
@@ -0,0 +1,39 @@
+﻿/*Жеребцов ККСО-01-19 НОД и НОК*/
+#include <stdio.h>
+#include <locale.h>
+
+int gcd(int a, int b)
+{
+	if (a < b)
+	{
+		a = a + b;
+		b = a - b;
+		a = a - b;
+	}
+
+	int r;
+	while ((a % b) != 0)
+	{
+		r = a % b;
+		a = b;
+		b = r;
+	}
+
+	return b;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+
+	int a, b;
+
+
+	printf("Введите два числа: ");
+	scanf("%d %d", &a, &b);
+
+	printf("\n(%d,%d) = %d\n", a, b, gcd(a, b));        // Алгоритм Евклида
+	printf("[%d,%d] = %d\n", a, b, (a * b) / gcd(a, b));// НОК(a,b) = (a*b) / НОД(a,b)
+	return 0;
+}
---
+++ Жеребцов ККСО-01-19 2 семестр/ДЗ на 25.03/Жеребцов ККСО-01-19 Скобки.c
@@ -0,0 +1,57 @@
+﻿/*Жеребцов ККСО-01-19 Скобки*/
+#include <stdio.h>
+#include <locale.h>
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int count_left = 0, count_right = 0;
+	int first_position_right = -1;
+
+	char str[60];
+	fgets(str, 60, stdin);
+
+
+	for (int i = 0; str[i] != '\0' || i < 60; i++)
+	{
+		if (str[i] == '(')
+		{
+			count_left++;
+
+			continue;
+		}
+
+		if (str[i] == ')')
+		{
+			count_right++;
+			if (first_position_right == -1)
+			{
+				first_position_right = i;
+			}
+
+			continue;
+		}
+	}
+
+	if (count_left == count_right)
+	{
+		printf("\nyes\n");
+	}
+	else
+	{
+		if (count_left > count_right)
+		{
+			printf("\nno\nexcess left bracket: count = %d", count_left);
+		}
+		else
+		{
+			printf("\nno\nexcess right bracket: first position = %d", first_position_right + 1);
+		}
+	}
+
+
+	return 0;
+}
