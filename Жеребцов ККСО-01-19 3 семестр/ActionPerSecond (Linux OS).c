3959ecb90aa7535ec74ff970d7497080cab95788
O33ero <58762519+O33ero@users.noreply.github.com>
2020-12-26 13:16:56 +0300

    HotFix

--- Жеребцов ККСО-01-19 3 семестр/ActionPerSecond (Linux OS).c
+++ Жеребцов ККСО-01-19 3 семестр/ActionPerSecond (Linux OS).c
@@ -5,27 +5,36 @@
 int main()
 {
 	FILE* f;
-	f = fopen("/dev/input/event2", "r");
+	f = fopen("/dev/input/event2", "r"); // Event-файл для клавиатуры
 
-	struct input_event k;
+	struct input_event k, u1, u2;                // Структура с ппоследней нажатой кнопкой
 
-	unsigned long int first_time;
+	unsigned long int first_time;        // Первая нажатая кнопка
 	int first_code = 0;
 
-	unsigned long int last_time;
+	unsigned long int last_time;         // Последняя (на данный момент) нажатая кнопка
 	int last_code = 0;
 
-	unsigned long int count = 0;
+ 	unsigned long int count = 0;         // Кол-во нажатий от начала работы
 
 	while(1)
 	{	
 		fread(&k, sizeof(k), 1, f);
-		fread(&k, sizeof(k), 1, f);
+		fread(&u1, sizeof(k), 1, f);
+		fread(&u2, sizeof(k), 1, f);
 
-		if (k.value == 0 || k.value == 1)
-			continue;
+
+		fread(&k, sizeof(k), 1, f);
+		fread(&u1, sizeof(k), 1, f);
+		fread(&u2, sizeof(k), 1, f);
 		
-//		printf("%d \n", k.value);
+
+
+
+		if (k.value == 1)
+		 	break;
+		
+		
 
 		if (first_code == 0)
 		{
@@ -38,14 +47,15 @@ int main()
 
 		count += 1;
 		
-//		printf("%ld \n\n", last_time);
-		printf("\n***\n c = %ld, t = %ld \n***\n", count,last_time-first_time ); 
+		system("clear");
+		printf("***\n c = %ld, t = %ld ", count,last_time-first_time ); 
 		
 
 		float t = (float)(last_time - first_time);
 		float c = (float)count;
 
-		printf("\n***\n Action per second = %f \n***\n", c/t );
+		printf("\n***\n Action per second = %f ", c/t );
+		printf("\n***\n Press ESC or CTR+Z to exit... \n***\n");
 
 	}
 
