5699121bbd606c505d8ef1d8843b4b5bbf18648d
O33ero <58762519+O33ero@users.noreply.github.com>
2021-01-28 21:47:11 +0300

    Ввод и вывод строк любых размеров (в данном случае 100)

---
+++ ASSambley/task15.asm
@@ -0,0 +1,39 @@
+section .bss
+    buffer resb 10  ; 
+        
+section .text:
+    global _start
+    
+_start:
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, buffer
+    mov edx, 100
+    int 0x80
+    
+    lea esi, [buffer]
+    mov ecx, eax
+    
+loop1:
+    push ecx
+
+    
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, esi
+    mov edx, 1
+    int 0x80
+    
+    add esi, 1 
+    
+    
+    pop ecx
+    loop loop1
+    
+        
+end:
+    
+
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
